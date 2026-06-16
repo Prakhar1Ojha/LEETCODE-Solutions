5a6068fad07bd8943a9124b659a9c3d26bbd0e4a
Prakhar1Ojha <pojha2737@gmail.com>
2026-06-16 11:21:14 +0530

    Create Java/Arrays/Array.java

---
+++ Java/Java/Arrays/Array.java
@@ -0,0 +1,10 @@
+# Arrays in Java
+
+This folder contains Java programs related to arrays, including:
+
+- Array declaration and initialization
+- Accessing array elements
+- Traversing arrays using loops
+- Searching and sorting basics
+- Single-dimensional and multi-dimensional arrays
+- Practice problems and examples
