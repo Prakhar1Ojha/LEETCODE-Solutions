7fe51ed8b700bbdecf3e13f763cbf8f081424c25
Prakhar1Ojha <pojha2737@gmail.com>
2026-06-25 11:25:53 +0530

    Add TwoSum class with user input functionality
    Implemented a TwoSum class with a method to find indices of two numbers that add up to a target. Added a main method for user input and output.

--- Java/Arrays/TwoSum.java
+++ Java/Arrays/TwoSum.java
@@ -1,1 +1,36 @@
+import java.util.*;
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+        TwoSum obj = new TwoSum();
+        int[] result = obj.twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution found");
+        }
+        sc.close();
+    }
+} 
 
